2e42359afcc490ac8d7f49e1d666e6373cf00cff
Shannon S <shannon.sandidge@gmail.com>
2017-02-04 17:46:12 -0700

    initial project version

--- app/secrets.py
+++ app/secrets.py
@@ -9,22 +9,22 @@ community = {
 }
 
 # This is a session secret key used by webapp2 framework.
-# Get 'a random and long string' from here: 
+# Get 'a random and long string' from here:
 # http://clsc.net/tools/random-string-generator.php
 # or execute this from a python shell: import os; os.urandom(64)
-SESSION_KEY = 
+SESSION_KEY = "\xa6EE\x01\x83\xa4\xcb\x87\x89\x81\xf0\xec\xe4'\x89=\xdc\x1a\xc1\xd1q\x9f \x1dt\xad\xfdY\x08]\x94\xf3N|\x83\x11^@v\xae\x98\t\xf5\xefB\xa8\\\xbce\x89\noGH\xb3>A\\\x9dvN\xd9\xa5K"
 
 # Google APIs
-GOOGLE_APP_ID = 
-GOOGLE_APP_SECRET = 
+GOOGLE_APP_ID ='400916539476-e6i1tbkpsf6s3vc6p08tmce3lsfu37sk.apps.googleusercontent.com'
+GOOGLE_APP_SECRET ='LGGdfAkj1OKT5_aQ7oGTJ14d'
 
 # Facebook auth apis
-FACEBOOK_APP_ID = 
-FACEBOOK_APP_SECRET = 
+FACEBOOK_APP_ID = '631641680299721'
+FACEBOOK_APP_SECRET = '5c0bcf44f37b53299855cf815a6e54266'
 
 # https://dev.twitter.com/apps
-TWITTER_CONSUMER_KEY = 
-TWITTER_CONSUMER_SECRET = 
+TWITTER_CONSUMER_KEY = 'LeQDWs29fMENzxtGcmTA6cC5Y'
+TWITTER_CONSUMER_SECRET = 'sb6pfGFTiew8MidvWo2rtuhJJ7S1Qk89mYGyVV97PdckarLQmR'
 
 
 # config that summarizes the above
--- readme.md
+++ readme.md
@@ -2,7 +2,8 @@
 
 * Create an application at [Google App Engine](http://appengine.google.com). Remember the application identifier.
 * To test locally, download the [App Engine SDK](https://developers.google.com/appengine/downloads). You will need the Python package for your OS.
-* Grab a copy of the Rideshare code by cloning or downloading a zip from [https://github.com/ysubtle/rideshare](https://github.com/ysubtle/rideshare).
+* Grab a copy of the Rideshare code by cloning or downloading a zip from
+[https://github.com/bnmnetp/rideshare](https://github.com/bnmnetp/rideshare) based on [https://github.com/ysubtle/rideshare](https://github.com/ysubtle/rideshare).
 * Rename secrets_template.py to secrets.py and follow the instructions.
 * Open app.yaml and change the application field to your application identifier that you set when creating your app on Google App Engine.
 * Upload the application to Google App Engine. You can do this by using the SDK.
